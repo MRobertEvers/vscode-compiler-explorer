553c82776733a930ed0579c5ac5f257bdbd6d72f
Matt <mevers@pivotint.com>
2020-03-07 01:25:14 -0600

    Allow config to change after init

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,20 @@
 import * as vscode from 'vscode';
 
-const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('compiler-explorer');
+function getConfig() : vscode.WorkspaceConfiguration {
+    return vscode.workspace.getConfiguration('compiler-explorer');
+}
 
 export function getCompilerExplorerHost() : string {
+    const config: vscode.WorkspaceConfiguration = getConfig();
     return config.get<string>('url');
 }
 
 export function getCompilerCode() : string {
+    const config: vscode.WorkspaceConfiguration = getConfig();
     return config.get<string>('compiler');
 }
 
 export function getCompilerOptions() : string {
+    const config: vscode.WorkspaceConfiguration = getConfig();
     return config.get<string>('options');
 }
